ded5e57470069ca6aba03131cacb61aa3a38b9a4
David Zhang <crispgm@gmail.com>
2017-06-12 14:48:39 +0800

    Setup mocha test

---
+++ test/server.test.js
@@ -0,0 +1,26 @@
+// test/server.test.js
+const exec = require('mz/child_process').exec;
+const request = require('supertest-as-promised');
+const expect = require('chai').expect;
+
+const app = require('../server/app');
+
+describe('builds application', function () {
+  it('builds to "build" directory', function () {
+    // Disable mocha time-out because this takes a lot of time
+    this.timeout(0);
+
+    // Run process
+    return exec('yarn build');
+  });
+});
+
+describe('express serving', function () {
+  it('responds to / with the index.html', function () {
+    return request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .then(res => expect(res.text).to.contain('<div id="root">'));
+  });
+});
