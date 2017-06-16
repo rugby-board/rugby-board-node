74f2ea3b73dab1b21ef5c3192d0747f00cf9e0c9
David Zhang <crispgm@gmail.com>
2017-06-16 16:20:39 +0800

    Add not found page

---
+++ client/components/NotFound.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Heading from './Heading.jsx';
+
+export default class NotFound extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const aboutHeading = {
+      id: '404',
+      title: '错误404',
+      more_text: '',
+      more_link: ''
+    };
+
+    this.state = {
+      aboutHeading: aboutHeading,
+    };
+  }
+
+  render() {
+    return (
+      <div className="columns">
+        <div className="column">
+          <Heading data={this.state.aboutHeading} />
+          <div className="article">
+            <div className="article-content">
+              <p>404 - 页面未找到</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
