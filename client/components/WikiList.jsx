8ef12e71c54578153265603185bb54ffc61d1e12
David Zhang <crispgm@gmail.com>
2017-07-02 01:14:19 +0800

    activeClass to wiki NavLink

--- client/components/WikiList.jsx
+++ client/components/WikiList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class WikiContentPage extends React.Component {
   render() {
@@ -9,17 +9,17 @@ export default class WikiContentPage extends React.Component {
           <div className="news-item">
             <h3>国家队赛事</h3>
             <ul>
-              <li><Link to="/wiki/rugby-championship">橄榄球冠军赛 The Rugby Championship</Link></li>
-              <li><Link to="/wiki/six-nations">欧洲六国赛 Six Nations</Link></li>
-              <li><Link to="/wiki/rugby-world-cup">橄榄球世界杯 Rugby World Cup</Link></li>
-              <li><Link to="/wiki/british-and-irish-lions">不列颠和爱尔兰狮子 British &amp; Irish Lions</Link></li>
+              <li><NavLink to="/wiki/rugby-championship" activeClassName="active-link">橄榄球冠军赛 The Rugby Championship</NavLink></li>
+              <li><NavLink to="/wiki/six-nations" activeClassName="active-link">欧洲六国赛 Six Nations</NavLink></li>
+              <li><NavLink to="/wiki/rugby-world-cup" activeClassName="active-link">橄榄球世界杯 Rugby World Cup</NavLink></li>
+              <li><NavLink to="/wiki/british-and-irish-lions" activeClassName="active-link">不列颠和爱尔兰狮子 British &amp; Irish Lions</NavLink></li>
             </ul>
             <h3>俱乐部赛事</h3>
             <ul>
-              <li><Link to="/wiki/super-rugby">超级橄榄球 Super Rugby</Link></li>
-              <li><Link to="/wiki/pro12-rugby">PRO12（前凯尔特联赛）</Link></li>
-              <li><Link to="/wiki/premiership-rugby">英格兰超级联赛 Premiership</Link></li>
-              <li><Link to="/wiki/top14-rugby">法国 TOP14 联赛</Link></li>
+              <li><NavLink to="/wiki/super-rugby" activeClassName="active-link">超级橄榄球 Super Rugby</NavLink></li>
+              <li><NavLink to="/wiki/pro12-rugby" activeClassName="active-link">PRO12（前凯尔特联赛）</NavLink></li>
+              <li><NavLink to="/wiki/premiership-rugby" activeClassName="active-link">英格兰超级联赛 Premiership</NavLink></li>
+              <li><NavLink to="/wiki/top14-rugby" activeClassName="active-link">法国 TOP14 联赛</NavLink></li>
             </ul>
           </div>
         </div>
--- client/components/wiki/Pro12.jsx
+++ client/components/wiki/Pro12.jsx
@@ -15,101 +15,101 @@ export default class Pro12 extends React.Component {
               </p>
               <p>
                 PRO12 目前参赛队伍分别是：
-                
-                <table className="wiki">
-                  <tr>
-                    <th>队名</th>
-                    <th>队名（英文）</th>
-                    <th>成立时间</th>
-                    <th>所在地</th>
-                    <th>夺冠次数</th>
-                  </tr>
-                  <tr>
-                    <td>贝纳通特雷维索</td>
-                    <td>Benetton Treviso</td>
-                    <td>1932</td>
-                    <td>意大利特雷维索</td>
-                    <td>0</td>
-                  </tr>
-                  <tr>
-                    <td>卡迪夫蓝</td>
-                    <td>Cardiff Blues</td>
-                    <td>2003</td>
-                    <td>威尔士卡迪夫</td>
-                    <td>0</td>
-                  </tr>
-                  <tr>
-                    <td>康诺特</td>
-                    <td>Connacht</td>
-                    <td>1885</td>
-                    <td>爱尔兰戈尔韦</td>
-                    <td>1 (2016)</td>
-                  </tr>
-                  <tr>
-                    <td>爱丁堡</td>
-                    <td>Edinburgh</td>
-                    <td>1872</td>
-                    <td>苏格兰爱丁堡</td>
-                    <td>0</td>
-                  </tr>
-                  <tr>
-                    <td>格拉斯哥勇士</td>
-                    <td>Glasgow Warriors</td>
-                    <td>1872</td>
-                    <td>苏格兰格拉斯哥</td>
-                    <td>1 (2015)</td>
-                  </tr>
-                  <tr>
-                    <td>伦斯特</td>
-                    <td>Leinster</td>
-                    <td>1879</td>
-                    <td>爱尔兰都柏林</td>
-                    <td>4 (2014)</td>
-                  </tr>
-                  <tr>
-                    <td>芒斯特</td>
-                    <td>Munster</td>
-                    <td>1879</td>
-                    <td>爱尔兰利莫瑞克和科克</td>
-                    <td>3 (2011)</td>
-                  </tr>
-                  <tr>
-                    <td>纽波特格文特龙</td>
-                    <td>Newport Gwent Dragons</td>
-                    <td>2003</td>
-                    <td>威尔士纽波特</td>
-                    <td>0</td>
-                  </tr>
-                  <tr>
-                    <td>鱼鹰</td>
-                    <td>Ospreys</td>
-                    <td>2003</td>
-                    <td>威尔士斯旺西</td>
-                    <td>4 (2012)</td>
-                  </tr>
-                  <tr>
-                    <td>深红</td>
-                    <td>Scarlets</td>
-                    <td>2003</td>
-                    <td>威尔士拉内利</td>
-                    <td>1 (2004)</td>
-                  </tr>
-                  <tr>
-                    <td>阿斯特</td>
-                    <td>Ulster</td>
-                    <td>1879</td>
-                    <td>爱尔兰贝尔法斯特</td>
-                    <td>1 (2006)</td>
-                  </tr>
-                  <tr>
-                    <td>斑马</td>
-                    <td>Zebre</td>
-                    <td>1973</td>
-                    <td>意大利帕尔马</td>
-                    <td>0</td>
-                  </tr>
-                </table>
               </p>
+              <table className="wiki">
+                <tr>
+                  <th>队名</th>
+                  <th>队名（英文）</th>
+                  <th>成立时间</th>
+                  <th>所在地</th>
+                  <th>夺冠次数</th>
+                </tr>
+                <tr>
+                  <td>贝纳通特雷维索</td>
+                  <td>Benetton Treviso</td>
+                  <td>1932</td>
+                  <td>意大利特雷维索</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>卡迪夫蓝</td>
+                  <td>Cardiff Blues</td>
+                  <td>2003</td>
+                  <td>威尔士卡迪夫</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>康诺特</td>
+                  <td>Connacht</td>
+                  <td>1885</td>
+                  <td>爱尔兰戈尔韦</td>
+                  <td>1 (2016)</td>
+                </tr>
+                <tr>
+                  <td>爱丁堡</td>
+                  <td>Edinburgh</td>
+                  <td>1872</td>
+                  <td>苏格兰爱丁堡</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>格拉斯哥勇士</td>
+                  <td>Glasgow Warriors</td>
+                  <td>1872</td>
+                  <td>苏格兰格拉斯哥</td>
+                  <td>1 (2015)</td>
+                </tr>
+                <tr>
+                  <td>伦斯特</td>
+                  <td>Leinster</td>
+                  <td>1879</td>
+                  <td>爱尔兰都柏林</td>
+                  <td>4 (2014)</td>
+                </tr>
+                <tr>
+                  <td>芒斯特</td>
+                  <td>Munster</td>
+                  <td>1879</td>
+                  <td>爱尔兰利莫瑞克和科克</td>
+                  <td>3 (2011)</td>
+                </tr>
+                <tr>
+                  <td>纽波特格文特龙</td>
+                  <td>Newport Gwent Dragons</td>
+                  <td>2003</td>
+                  <td>威尔士纽波特</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>鱼鹰</td>
+                  <td>Ospreys</td>
+                  <td>2003</td>
+                  <td>威尔士斯旺西</td>
+                  <td>4 (2012)</td>
+                </tr>
+                <tr>
+                  <td>深红</td>
+                  <td>Scarlets</td>
+                  <td>2003</td>
+                  <td>威尔士拉内利</td>
+                  <td>1 (2004)</td>
+                </tr>
+                <tr>
+                  <td>阿斯特</td>
+                  <td>Ulster</td>
+                  <td>1879</td>
+                  <td>爱尔兰贝尔法斯特</td>
+                  <td>1 (2006)</td>
+                </tr>
+                <tr>
+                  <td>斑马</td>
+                  <td>Zebre</td>
+                  <td>1973</td>
+                  <td>意大利帕尔马</td>
+                  <td>0</td>
+                </tr>
+              </table>
+              
               <p>
                 PRO12 联赛球队实力相差比较明显，其中爱尔兰最强。爱尔兰传统三强（伦斯特、芒斯特、阿斯特）一直成绩不俗，2016年原先较弱的康诺特也一举夺冠，爱尔兰球队总共获得了总共15届联赛中9次冠军。并且，伦斯特还多次获得原来的欧冠前身喜力杯的冠军。
               </p>
