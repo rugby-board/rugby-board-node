df45daf649c2a5939c7ee2d18e78e2211e15fa24
David Zhang <crispgm@gmail.com>
2017-07-27 20:52:16 +0800

    Use loading animation

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -1,12 +1,14 @@
 import { h, Component } from 'preact';
 
+import LoadingImg from '../assets/loading.svg';
+
 export default class Loading extends Component {
   render() {
     return (
       <div className="columns">
         <div className="column">
           <div className="loading">
-            {this.props.text}
+            <img src={LoadingImg} alt={this.props.text} />
           </div>
         </div>
       </div>
