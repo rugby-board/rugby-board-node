fb1fa2a9560a96c1a139e5ad9502a87db702cd93
David Zhang <crispgm@gmail.com>
2017-08-08 11:27:58 +0800

    Make UnsplashImage a component

--- client/pages/AboutPage.jsx
+++ client/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 
 import { setDocumentTitle } from '../util';
 import Heading from '../components/Heading';
+import UnsplashImage from '../components/UnsplashImage';
 
 export default class AboutPage extends Component {
   constructor(props) {
@@ -21,20 +22,27 @@ export default class AboutPage extends Component {
   }
 
   render() {
-    /* eslint-disable react/style-prop-object */
     return (
       <div>
         <Heading data={this.state.aboutHeading} />
         <div className="columns">
-          <div className="column column-1-2">
+          <div className="column column-2-3">
             <div className="article">
               <div className="article-content">
                 <h3>Rugby News Board 中文英式橄榄球资讯</h3>
                 <p>
                   Copyright &copy; <a href="https://crispgm.com">David Zhang</a>.
-                  <br />
-                  联系请发邮件: crispgm at gmail.com
+                  联系请发邮件: crispgm at gmail.com.
                 </p>
+
+                <h3>什么是 Rugby News Board?</h3>
+                <p>
+                  可能是世界上唯一一家坚持更新的中文英式橄榄球资讯网站。
+                </p>
+                <p>
+                  整个网站开发、维护、编辑和运营，均由站长 David Zhang 独立完成。由于没有足够时间编译，Rugby News Board 只提供简洁的第一手资讯，如果想深入阅读请直接阅读英文或者去『橄榄球资料』中寻找中文长内容的链接。
+                </p>
+
                 <h3>为什么会有 Rugby News Board?</h3>
                 <p>
                   英式橄榄球在国内人气真是少，既没有足球、乒乓球一样的群众基础，也没有 NBA、NFL 一样的商业推广，还因为世界比赛、奥运会没啥前途也不被总菊支持。我曾是一个在澳大利亚的留学生，前往澳大利亚之前先了解一些当地的文化，其中有一块就是橄榄球。在澳大利亚，Rugby Union、Rugby League 和 Australian Football 三种“橄榄球”都很受欢迎。
@@ -55,26 +63,32 @@ export default class AboutPage extends Component {
               </div>
             </div>
           </div>
-          <div className="column column-1-2">
+          <div className="column column-1-3">
             <div className="article">
               <div className="article-content">
                 <p>
-                  <img src="/public/quino-al-155743.jpg" alt="Rugby Sports" />
-                  <a
-                    style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px;"
-                    href="https://unsplash.com/@quinoal?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Download free do whatever you want high-resolution photos from Quino Al"
-                  >
-                    <span style="display:inline-block;padding:2px 3px;">
-                      <svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white;" viewBox="0 0 32 32">
-                        <title />
-                        <path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z" />
-                      </svg>
-                    </span>
-                    <span style="display:inline-block;padding:2px 3px;">Quino Al</span>
-                  </a>
+                  <UnsplashImage 
+                    src="public/quino-al-155743.jpg"
+                    alt="Rugby Moment 1"
+                    author="Quino Al"
+                    username="quinoal"
+                  />
+                </p>
+                <p>
+                  <UnsplashImage 
+                    src="public/quino-al-197082.jpg"
+                    alt="Rugby Moment 2"
+                    author="Quino Al"
+                    username="quinoal"
+                  />
+                </p>
+                <p>
+                  <UnsplashImage 
+                    src="public/quino-al-195526.jpg"
+                    alt="Rugby Moment 3"
+                    author="Quino Al"
+                    username="quinoal"
+                  />
                 </p>
               </div>
             </div>
