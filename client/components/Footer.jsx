5a0aa1dc39f25affde73db3086e335b2dbf44e05
David Zhang <crispgm@gmail.com>
2018-06-07 18:00:11 +0800

    Fix footer typo

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -24,7 +24,7 @@ export default class Footer extends Component {
             <div className="menu-item"><Link href="/event/premiership">Premiership</Link></div>
             <div className="menu-item"><Link href="/event/top14">Top 14</Link></div>
             <div className="menu-item"><Link href="/event/pro14">PRO 14</Link></div>
-            <div className="menu-item"><Link href="/event/international-tests">国家测试赛</Link></div>
+            <div className="menu-item"><Link href="/event/international-tests">国际测试赛</Link></div>
             <div className="menu-item"><Link href="/event/rugby-championship">冠军赛</Link></div>
             <div className="menu-item"><Link href="/event/six-nations">六国赛</Link></div>
             <div className="menu-item"><Link href="/event/rugby-world-cup">世界杯</Link></div>
